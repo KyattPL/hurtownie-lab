bc958c4f44244b25d0397032484c203cf73f443c
KyattPL <siema1234@gazeta.pl>
2022-03-10 01:12:06 +0100

    updated exercise 1

--- lista1/1_3.sql
+++ lista1/1_3.sql
@@ -1,3 +1,15 @@
+CREATE TABLE Adres (
+	idA INTEGER PRIMARY KEY,
+	miejscowosc VARCHAR(50) NOT NULL,
+	gmina VARCHAR(50) NOT NULL,
+	powiat VARCHAR(50) NOT NULL,
+	wojewodztwo VARCHAR(50) NOT NULL,
+	ulica VARCHAR(50) NOT NULL,
+	kodPocztowy INTEGER NOT NULL,
+	nrDomu INTEGER NOT NULL,
+	nrMieszkania INTEGER
+);
+
 CREATE TABLE Klient (
 	idK INTEGER PRIMARY KEY,
 	imie VARCHAR(50) NOT NULL,
@@ -6,13 +18,13 @@ CREATE TABLE Klient (
 	wiek INTEGER,
 	email VARCHAR(30) NOT NULL UNIQUE,
 	hashHasla VARCHAR(256) NOT NULL UNIQUE,
-	adresDostawy INTEGER FOREIGN KEY UNIQUE
+	adresDostawy INTEGER FOREIGN KEY REFERENCES Adres(idA) UNIQUE
 );
 
 CREATE TABLE Sklep (
 	idS INTEGER PRIMARY KEY,
-	nazwaS STRING NOT NULL,
-	adresSklepu INTEGER FOREIGN KEY NOT NULL UNIQUE
+	nazwaS VARCHAR(30) NOT NULL,
+	adresSklepu INTEGER FOREIGN KEY REFERENCES Adres(idA) NOT NULL UNIQUE
 );
 
 CREATE TABLE Kategoria (
@@ -25,7 +37,7 @@ CREATE TABLE Produkt(
 	kategoriaP VARCHAR(30) REFERENCES Kategoria(nazwaKat) NOT NULL,
 	cenaP DECIMAL(30,4) NOT NULL,
 	wagaP REAL,
-	rokProdukcji DATE
+	rokProdukcji INTEGER
 );
 
 CREATE TABLE ProduktSklepowy(
@@ -38,12 +50,11 @@ CREATE TABLE RobiZakupy (
 	nrTransakcji INTEGER PRIMARY KEY,
 	idKlienta INTEGER REFERENCES Klient(idK) NOT NULL,
 	idSklepu INTEGER REFERENCES Sklep(idS) NOT NULL,
-	dataZ DATE NOT NULL,
 	czasZ TIME NOT NULL,
 	dataZamowienia DATE NOT NULL,
 	dataOplacenia DATE,
 	dataOdbioru DATE,
-	calkowitaSuma DECIMAL(30,4) NOT NULL;
+	calkowitaSuma DECIMAL(30,4) NOT NULL
 );
 
 CREATE TABLE Nabywa (
